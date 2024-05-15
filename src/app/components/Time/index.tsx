1f1336523fb740f96b3ea0fd5993badc5c26ac42
Victor Alvar <victor.alvar@tutanota.com>
2024-05-15 16:48:11 -0300

    Add update components.

---
+++ src/app/components/Time/index.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import {format} from 'date-fns'
+import {useEffect, useState} from 'react'
+
+type TimeProps = {
+  time: Date
+}
+
+function Time({time}: TimeProps) {
+  const [formattedDate, setFormattedDate] = useState('')
+
+  useEffect(() => setFormattedDate(format(new Date(time), 'h:mm a')), [time])
+
+  return <time suppressHydrationWarning={true}>{formattedDate}</time>
+}
+
+export default Time
