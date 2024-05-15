ba2bd2b6aba32abf9cab60a19dfe4cce4266071d
Victor Alvar <victor.alvar@tutanota.com>
2024-05-15 18:53:40 -0300

    Add pages game and standings.

---
+++ src/app/(pages)/game/[id]/page.tsx
@@ -0,0 +1,51 @@
+import {format} from 'date-fns/format'
+
+import SWRProvider from '@/app/components/SWRProvider'
+import {GameDetails} from '@/app/components'
+import {DATE_TITLE_FORMAT} from '@/app/constants'
+
+async function getData(id: string) {
+  const res = await fetch(`${process.env.API_DOMAIN}/api/game/${id}`, {
+    cache: 'no-store',
+  })
+
+  const data = await res.json()
+  return data
+}
+
+type GameDetailsProps = {
+  params: {
+    id: string
+  }
+}
+
+export async function generateMetadata({params}: GameDetailsProps) {
+  const data = await getData(params.id)
+  const homeTeam = data.game?.homeTeam.teamName
+  const awayTeam = data.game?.awayTeam.teamName
+  const date = new Date(data.game?.gameTimeUTC)
+
+  if (data.game) {
+    return {
+      title: `${awayTeam} x ${homeTeam} | NBA Next.JS`,
+      description: `See ${awayTeam} x ${homeTeam} results for the game on ${format(
+        date,
+        DATE_TITLE_FORMAT
+      )}`,
+    }
+  }
+}
+
+export default async function Page({params: {id}}: GameDetailsProps) {
+  const data = await getData(id)
+
+  const fallback = {
+    [`/api/game/${id}`]: data,
+  }
+
+  return (
+    <SWRProvider fallback={fallback}>
+      <GameDetails id={id} />
+    </SWRProvider>
+  )
+}
---
+++ src/app/(pages)/standings/page.tsx
@@ -0,0 +1,44 @@
+import {API} from '@/app/constants'
+import {SwitchConference} from '@/app/components'
+import {getLeagueYear, conferenceExtractor} from '@/app/helpers'
+import {Metadata} from 'next'
+import {Suspense} from 'react'
+
+async function getData() {
+  const year = getLeagueYear(new Date())
+  const nextYear = (year + 1) % 100
+  const season = `${year}-${nextYear}`
+
+  const res = await fetch(
+    `${API.BASE_URL}/leaguestandingsv3&GroupBy=conf&LeagueID=00&Season=${season}&SeasonType=Regular%20Season&Section=overall`,
+    {
+      next: {
+        revalidate: 60,
+      },
+    }
+  )
+
+  const data = await res.json()
+
+  return {
+    east: conferenceExtractor(data.resultSets[0].rowSet, true),
+    west: conferenceExtractor(data.resultSets[0].rowSet, false),
+  }
+}
+
+export const metadata: Metadata = {
+  title: 'Standings | NBA Next.js',
+  description: 'See the current standings for NBA',
+}
+
+async function Standings() {
+  const {east, west} = await getData()
+
+  return (
+    <Suspense>
+      <SwitchConference east={east} west={west} />
+    </Suspense>
+  )
+}
+
+export default Standings
