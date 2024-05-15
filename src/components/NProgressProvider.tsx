07157e998a629359e8455a06ef30e056bdceead9
Victor Alvar <victor.alvar@tutanota.com>
2024-05-15 11:21:43 -0300

    Add update page.tsx and layout.

---
+++ src/components/NProgressProvider.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import {Suspense} from 'react'
+import {AppProgressBar as ProgressBar} from 'next-nprogress-bar'
+
+const NProgressProvider = ({children}: {children: React.ReactNode}) => {
+  return (
+    <Suspense>
+      {children}
+      <ProgressBar
+        height="4px"
+        color="rgb(96 165 250)"
+        options={{showSpinner: false}}
+        shallowRouting
+      />
+    </Suspense>
+  )
+}
+
+export default NProgressProvider
