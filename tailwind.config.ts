1e8cfe0cb7d308e5f5befa623ca70b6e1dd70514
Victor Alvar <victor.alvar@tutanota.com>
2024-05-15 19:52:36 -0300

    Add update postcss and tailwind config files.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      gridTemplateColumns: {
+        'auto-fill': 'repeat(auto-fill, minmax(320px, 1fr))',
+      },
     },
   },
   plugins: [],
