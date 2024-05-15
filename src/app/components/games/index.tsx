a2e317c03e4f5dd88c5ea8dba2c0783144a59aee
Victor Alvar <victor.alvar@tutanota.com>
2024-05-15 16:26:20 -0300

    Add update components, helpers, api, and add hooks.

--- src/app/components/games/index.tsx
+++ src/app/components/games/index.tsx
@@ -1,9 +1,25 @@
 'use client'
 
+import useSWR from 'swr'
+
+import {DateSelector, GamesList} from '@/app/components'
+import {getDays, ParsedGames} from '@/app/helpers'
+
+const fetcher = (...args: [RequestInfo, RequestInit]) =>
+  fetch(...args).then((res) => res.json())
+
 function Games() {
+  const {day, prevDay, nextDay} = getDays()
+  const {data} = useSWR<ParsedGames>('/api/today', fetcher, {
+    refreshInterval: 15000,
+  })
+
+  console.log('Hello here', data)
+
   return (
     <>
-      <div>Hello World!</div>
+      <DateSelector day={day} prevDay={prevDay} nextDay={nextDay} />
+      <GamesList games={data} />
     </>
   )
 }
